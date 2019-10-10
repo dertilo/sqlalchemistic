2ecf0931b798f12fd1052c7a604090893b6c3ee2
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-10-10 21:14:21 +0200

    semantic scholar to postgres

---
+++ semanticscholar_to_sqldatabase.py
@@ -0,0 +1,76 @@
+from pprint import pprint
+from time import time
+
+from multiprocessing_pooling import consume_parallel
+from sqlutil.sqlalchemy_methods import count_rows, get_tables_by_reflection
+from sqlutil.sqlalchemy_methods import create_sqlalchemy_base_engine
+from util import util_methods, data_io
+from sqlalchemy import Integer, Float, Table, String, Column
+
+def populate_table(sqlalchemy_engine,table, data_g,batch_size=1000):
+    data_g = (fill_missing_with_Nones(l, column_names) for l in data_g)
+    with sqlalchemy_engine.connect() as conn:
+        util_methods.consume_batchwise(lambda rows:conn.execute(table.insert(), rows), data_g, batch_size)
+
+def fill_missing_with_Nones(l, column_names):
+    return {k: l.get(k, None) for k in column_names + ['id']}
+
+def build_consumer_supplier(limit,batch_size): ## called in main
+    def consumer_supplier(): ## called once on worker
+
+        sqlalchemy_base, sqlalchemy_engine = create_sqlalchemy_base_engine(
+            dburl='postgresql://postgres:postgres@localhost:5432/postgres')
+
+        table_name = 'semanticscholar'
+        tables = get_tables_by_reflection(sqlalchemy_base.metadata, sqlalchemy_engine)
+        assert table_name in tables
+        table = tables[table_name]
+        column_names = [c.name for c in table.columns]
+
+        def consumer(file:str):## called multiple times on worker
+            data_g = (fill_missing_with_Nones(d, column_names) for d in
+                      data_io.read_jsonl(file, limit=limit, num_to_skip=0))
+            populate_table(sqlalchemy_engine, table, data_g, batch_size=batch_size)
+
+        return consumer
+    return consumer_supplier
+
+if __name__ == "__main__":
+    import os
+
+    path = '/docker-share/data/semantic_scholar'
+
+    files = [path + '/' + file_name for file_name in os.listdir(path) if file_name.startswith('s2') and file_name.endswith('.gz')]
+    num_processes=8
+    these_files = files[:num_processes]
+
+    column_names = ['title','paperAbstract','year']
+    columns = [Column('id', String, primary_key=True)] + [Column(colname, String(),nullable=True) for colname in column_names]
+
+    sqlalchemy_base, sqlalchemy_engine = create_sqlalchemy_base_engine(dburl='postgresql://postgres:postgres@localhost:5432/postgres')
+
+    table_name = 'semanticscholar'
+    tables = get_tables_by_reflection(sqlalchemy_base.metadata, sqlalchemy_engine)
+    pprint(tables)
+    if table_name in tables:
+        table = tables[table_name]
+        table.drop(sqlalchemy_engine)
+    table = Table(table_name, sqlalchemy_base.metadata, *columns, extend_existing=True)
+    table.create()
+    print('populating: %s'%table_name)
+    limit = 800_000
+    batch_size = 1_000
+
+    start = time()
+    consumer = build_consumer_supplier(limit=limit, batch_size=batch_size)()
+    [consumer(file) for file in files]
+    numrows = count_rows(sqlalchemy_engine,table)
+    dur = time() - start
+    print('with %d processes inserted %d rows in %0.2f secs with speed of %0.2f rows/sec' % (0,numrows,dur,float(numrows)/dur))
+
+
+    start = time()
+    consume_parallel(these_files, build_consumer_supplier(limit=int(limit/num_processes), batch_size=batch_size), num_processes=num_processes)
+    numrows = count_rows(sqlalchemy_engine,table)
+    dur = time() - start
+    print('with %d processes inserted %d rows in %0.2f secs with speed of %0.2f rows/sec' % (num_processes,numrows,dur,float(numrows)/dur))
